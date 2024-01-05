8cec87147dc0836530855a7d8b7ee08a87e98923
Abigail-designs <abigail.prendergast20@gmail.com>
2024-01-05 16:56:22 -0600

    css test

---
+++ Basic_HTML_and_CSS/debug/Hello_World.js
@@ -0,0 +1,7 @@
+var greeting ='Hello world';
+console.log(greeting);
+
+var greeting2 = 'Welcome to Node.js';
+console.log(greeting2);
+var sentence1 = 'Learning to code with the debugger';
+console.log(sentence0ne);
